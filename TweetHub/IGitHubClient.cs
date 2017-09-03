70d1ca625deade6ea7d3bd7f7ac44406e20768d7
DESKTOP-3K4Q64N\Kohei <wipiano@gmail.com>
2017-09-03 10:25:12 +0900

    add GitHubClient interface

---
+++ TweetHub/IGitHubClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TweetHub.Models;
+
+namespace TweetHub
+{
+    public interface IGitHubClient
+    {
+        Task<IEnumerable<GitHubRepository>> GetUserRepositoriesAsync();
+    }
+}
